b195b0970bffc8b2ffc1a9df547b0a31375a1dc5
james <james@DESKTOP-638L93L>
2023-11-14 16:04:36 +0800

    new feature: change font size from UI

---
+++ EyesOnU.Service/Extension/ConvertExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyesOnU.Service.Extension
+{
+    public static class ConvertExtension
+    {
+        public static int GetInt(this string key, int defaultValue = 0)
+        {
+            if (int.TryParse(key, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                Debug.WriteLine($"[Int Parse failed] {key}");
+                return defaultValue;
+            }
+        }
+        public static float GetFloat(this string key, float defaultValue = 0)
+        {
+            if (float.TryParse(key, out float result))
+            {
+                return result;
+            }
+            else
+            {
+                Debug.WriteLine($"[Int Parse failed] {key}");
+                return defaultValue;
+            }
+        }
+    }
+}
--- EyesOnU/Monitor.cs
+++ EyesOnU/Monitor.cs
@@ -13,6 +13,8 @@ namespace EyesOnU
 {
     public partial class Monitor : FormBorderlessAndAlwaysTop
     {
+        const float MAXFont = 60;
+        const float MINFont = 5;
         private static readonly Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
         SystemMonitorService systemMonitorService = SystemMonitorService.Instance;
@@ -26,11 +28,36 @@ namespace EyesOnU
             }
             set
             {
-                var parsedValue = GetInt(value, 100);
+                var parsedValue = value.GetInt(100);
                 config.AppSettings.Settings["RefreshRate"].Value = parsedValue.ToString();
                 config.Save(ConfigurationSaveMode.Modified);
                 ConfigurationManager.RefreshSection("appSettings");
                 CounterList.ForEach(each => { each.RefreshRate = parsedValue; });
+            }
+        }
+
+        public float FontSize
+        {
+            get
+            {
+                return config.AppSettings.Settings["FontSize"].Value.GetFloat(9);
+            }
+            set
+            {
+                value = value < MINFont ? MINFont : value;
+                value = value > MAXFont ? MAXFont : value;
+
+                config.AppSettings.Settings["FontSize"].Value = value.ToString();
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+                pnlContent.Controls.OfType<Label>().ToList().ForEach(each => { each.Font = new Font(each.Font.FontFamily, value, each.Font.Style, each.Font.Unit, ((byte)(0))); });
+
+                var allElement = GetAllControls(pnlContent);
+                var bottomContent = allElement.Where(f => f is Label).Max(m => m.Bottom);
+                allElement.Where(f => f is not Label).ToList().ForEach(each =>
+                {
+                    each.Top = bottomContent;
+                });
             }
         }
 
@@ -50,25 +77,14 @@ namespace EyesOnU
                 return ColorTranslator.FromHtml(color);
             }
         }
-        public int GetInt(string key, int defaultValue = 0)
-        {
-            if (int.TryParse(key, out int result))
-            {
-                return result;
-            }
-            else
-            {
-                Debug.WriteLine($"[Int Parse failed] {key}");
-                return defaultValue;
-            }
-        }
+
         public Monitor()
         {
             InitializeComponent();
             BackColor = SettingBackColor;
             this.Shown += (s, e) =>
             {
-                var refreshRate = GetInt(RefreshRate);
+                var refreshRate = RefreshRate.GetInt();
                 CounterList.ForEach(each => { Task.Factory.StartNew(() => { each.StartNext(refreshRate); }); });
                 pnlContent.Controls.OfType<CheckBox>().FirstOrDefault()?.Focus();
             };
@@ -87,11 +103,12 @@ namespace EyesOnU
                 {
                     AutoSize = true,
                     Text = "Initializing...",
-                    Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
+                    Font = new Font("Consolas", FontSize, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0))),
                     BackColor = SettingBackColor,
                     ForeColor = SettingForeColor,
                     Location = new Point(0, yPos),
                 };
+                //label.Font = new Font(label.Font.FontFamily, 15, label.Font.Style, label.Font.Unit, ((byte)(0)));
                 each.ValueUpdated += (s, e) =>
                 {
                     if (s is CounterMonitor data)
@@ -120,7 +137,7 @@ namespace EyesOnU
             {
                 BackColor = SettingBackColor,
                 ForeColor = SettingForeColor,
-                Width = 60,
+                Width = 40,
                 Text = RefreshRate,
                 Location = new Point(0, yPos),
                 TextAlign = HorizontalAlignment.Right,
@@ -128,6 +145,7 @@ namespace EyesOnU
             txtRefreshRate.TextChanged += (s, e) =>
             {
                 txtRefreshRate.Text = Regex.Replace(txtRefreshRate.Text, @"[^0-9]", "");
+                RefreshRate = txtRefreshRate.Text;
             };
             txtRefreshRate.KeyPress += (s, e) =>
             {
@@ -136,10 +154,6 @@ namespace EyesOnU
                     e.Handled = true;
                 }
             };
-            txtRefreshRate.TextChanged += (s, e) =>
-            {
-                RefreshRate = txtRefreshRate.Text;
-            };
             this.pnlContent.Controls.Add(txtRefreshRate);
             #endregion
             #region CheckBox TopMost
@@ -147,9 +161,9 @@ namespace EyesOnU
             {
                 BackColor = SettingBackColor,
                 ForeColor = SettingForeColor,
-                Width = 80,
+                Width = 60,
                 TextAlign = ContentAlignment.MiddleCenter,
-                Text = "TopMost",
+                Text = "Top",
                 Checked = this.TopMost,
                 Location = new Point(txtRefreshRate.Right + 10, yPos),
             };
@@ -159,13 +173,41 @@ namespace EyesOnU
             };
             this.pnlContent.Controls.Add(chkLock);
             #endregion
+            #region TextBox RefreshRate
+            TextBox txtFontSize = new TextBox
+            {
+                BackColor = SettingBackColor,
+                ForeColor = SettingForeColor,
+                Width = 30,
+                Text = FontSize.ToString(),
+                TextAlign = HorizontalAlignment.Right,
+                Location = new Point(chkLock.Right + 10, yPos),
+            };
+            txtFontSize.TextChanged += (s, e) =>
+            {
+                txtFontSize.Text = txtFontSize.Text.GetFloat(0).ToString();
+            };
+            txtFontSize.KeyPress += (s, e) =>
+            {
+                if (e.KeyChar == (char)Keys.Enter && txtFontSize.Text.GetFloat() != FontSize && MessageBox.Show("Restart to Apply?", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    FontSize = txtFontSize.Text.GetFloat();
+                    Application.Restart();
+                }
+                else if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            };
+            this.pnlContent.Controls.Add(txtFontSize);
+            #endregion
             #region Button Exit
             Button btnExit = new Button
             {
                 BackColor = SettingBackColor,
                 ForeColor = SettingForeColor,
                 Text = "Exit",
-                Location = new Point(chkLock.Right + 10, yPos),
+                Location = new Point(txtFontSize.Right + 10, yPos),
             };
             btnExit.Click += (s, e) =>
             {
